06bd4eb2ae246ada1088ed742ff9ec0c6231cdb7
zhudi <zhudi@163.com>
2022-01-09 22:57:52 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/atguigu/git/GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
         System.out.println("push test+");
         System.out.println("pull test");
         System.out.println("更新中...");
+        System.out.println("更新中1...");
     }
 }
